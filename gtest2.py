c295d6134723a702a3138ac7978694a72d474da0
xssddq <xss_ddq@163.com>
2018-12-06 16:58:37 +0800

    Create gtest2.py

---
+++ gtest2.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        "HOST": '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        "PASSWORD": 'gtest_1111112222233333',
+        'NAME': 'node',
+        'TEST': {'CHARSET': 'UTF8'}
+    }
+}
